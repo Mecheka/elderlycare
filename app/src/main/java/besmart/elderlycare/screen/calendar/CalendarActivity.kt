98ff7ad3f2ba59b8b1858155d61fddb592e94b64
Suriya <mcmio125@gmail.com>
2019-07-20 00:58:44 +0700

    implement calendar

--- app/src/main/java/besmart/elderlycare/screen/calendar/CalendarActivity.kt
+++ app/src/main/java/besmart/elderlycare/screen/calendar/CalendarActivity.kt
@@ -3,11 +3,22 @@ package besmart.elderlycare.screen.calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import besmart.elderlycare.R
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import kotlinx.android.synthetic.main.activity_calendar.*
+import kotlinx.android.synthetic.main.activity_calendar.toolbar
+import kotlinx.android.synthetic.main.activity_select_user_type.*
 
 class CalendarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+        calendar.selectedDate = CalendarDay.today()
     }
 }
