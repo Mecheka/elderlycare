0e26aa20a56294437566c26c3450b7a5e92ca385
Suriya <mcmio125@gmail.com>
2019-07-21 23:41:14 +0700

    add elderly info

---
+++ app/src/main/java/besmart/elderlycare/screen/elderlyinfo/ElderlyInfoActivity.kt
@@ -0,0 +1,13 @@
+package besmart.elderlycare.screen.elderlyinfo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import besmart.elderlycare.R
+
+class ElderlyInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_elderly_info)
+    }
+}
