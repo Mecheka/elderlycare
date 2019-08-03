563d9955a20b4fb524c7d4c85c30f76eb096a7a1
Suriya <mcmio125@gmail.com>
2019-08-03 22:14:35 +0700

    add evaluation

---
+++ app/src/main/java/besmart/elderlycare/screen/evaluation/EvaluationActivity.kt
@@ -0,0 +1,13 @@
+package besmart.elderlycare.screen.evaluation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import besmart.elderlycare.R
+
+class EvaluationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_evaluation)
+    }
+}
