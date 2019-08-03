afb89d0b7648e1877a24e17486fe7bc46dbf9934
Suriya <mcmio125@gmail.com>
2019-08-03 13:07:27 +0700

    check user exit

--- app/src/main/java/besmart/elderlycare/screen/splach/SplachScreenActivity.kt
+++ app/src/main/java/besmart/elderlycare/screen/splach/SplachScreenActivity.kt
@@ -1,21 +1,34 @@
 package besmart.elderlycare.screen.splach
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import besmart.elderlycare.R
+import besmart.elderlycare.model.login.LoginResponce
+import besmart.elderlycare.model.profile.ProfileResponce
+import besmart.elderlycare.screen.main.MainActivity
 import besmart.elderlycare.screen.welcome.WelcomeActivity
+import besmart.elderlycare.util.Constance
+import com.orhanobut.hawk.Hawk
 import kotlinx.android.synthetic.main.activity_splach_screen.*
 
 class SplachScreenActivity : AppCompatActivity() {
+
+    private val profile = Hawk.get<ProfileResponce>(Constance.USER)
+    private val auth = Hawk.get<LoginResponce>(Constance.TOKEN)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splach_screen)
 
         imageView.postDelayed( {
-            startActivity(Intent(this, WelcomeActivity::class.java))
-            finish()
+            if(profile == null && auth == null) {
+                startActivity(Intent(this, WelcomeActivity::class.java))
+                finish()
+            }else{
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
         }, 1000)
     }
 }
---
+++ app/src/main/java/besmart/elderlycare/util/ItemAdapter.kt
@@ -0,0 +1,4 @@
+package besmart.elderlycare.util
+
+interface ItemAdapter {
+}
