5c254fd2cb94232a271e584b530eb40d6358636f
Suriya <mcmio125@gmail.com>
2019-08-15 23:58:52 +0700

    fix string and map

--- app/src/main/java/besmart/elderlycare/screen/gps/MapsFragment.kt
+++ app/src/main/java/besmart/elderlycare/screen/gps/MapsFragment.kt
@@ -1,20 +1,38 @@
 package besmart.elderlycare.screen.gps
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.IntentSender
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import besmart.elderlycare.R
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
-class MapsFragment : Fragment(), OnMapReadyCallback {
+class MapsFragment : Fragment() {
 
-    private lateinit var mMap: GoogleMap
+    private var mMap: GoogleMap? = null
+    private var mGoogleApiClient: GoogleApiClient? = null
+    private var mFusedLoction: FusedLocationProviderClient? = null
+    private var mLocationRequest: LocationRequest? = null
+    private val mLocationCallBack: LocationCallback? = null
+    private val REQEUST_LOCATION = 701
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +45,43 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        try {
+            mFusedLoction = LocationServices.getFusedLocationProviderClient(context!!)
+            mLocationRequest = LocationRequest.create()
+            mLocationRequest?.interval = 5000
+            mLocationRequest?.fastestInterval = 1000
+            mLocationRequest?.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            val manager = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+            if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER) && hasGPSDevice(activity!!)) {
+                Log.e("keshav", "Gps already enabled")
+                Toast.makeText(activity, "Gps not enabled", Toast.LENGTH_SHORT).show()
+                enableLoc()
+            } else {
+                Log.e("keshav", "Gps already enabled")
+                Toast.makeText(activity, "Gps already enabled", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        if (mMap == null) {
+            val mapFragment = childFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+            mapFragment.getMapAsync { googleMap -> createMaps(googleMap) }
+        }
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (mGoogleApiClient != null) {
+            try {
+                mFusedLoction?.removeLocationUpdates(mLocationCallBack)
+            } catch (e: NullPointerException) {
+                e.printStackTrace()
+            }
+        }
     }
 
     /**
@@ -38,12 +93,90 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    override fun onMapReady(googleMap: GoogleMap) {
+    private fun createMaps(googleMap: GoogleMap) {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val sydney = LatLng(13.8513962850, 100.6877681210)
+        mMap?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap?.animateCamera(CameraUpdateFactory.zoomTo(12f))
+
+        Dexter.withActivity(activity)
+            .withPermissions(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+            .withListener(object : MultiplePermissionsListener {
+                @SuppressLint("MissingPermission")
+                override fun onPermissionsChecked(report: MultiplePermissionsReport) {
+                    mMap?.isMyLocationEnabled = true
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: List<PermissionRequest>,
+                    token: PermissionToken
+                ) {
+
+                }
+            }).check()
+    }
+
+    private fun hasGPSDevice(context: Context): Boolean {
+        val mgr = context
+            .getSystemService(Context.LOCATION_SERVICE) as LocationManager ?: return false
+        val providers = mgr.allProviders ?: return false
+        return providers.contains(LocationManager.GPS_PROVIDER)
+    }
+
+    private fun enableLoc() {
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = GoogleApiClient.Builder(context!!)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(object : GoogleApiClient.ConnectionCallbacks {
+                    override fun onConnected(bundle: Bundle?) {
+
+                    }
+
+                    override fun onConnectionSuspended(i: Int) {
+                        mGoogleApiClient?.connect()
+                    }
+                })
+                .addOnConnectionFailedListener { connectionResult ->
+                    Log.d(
+                        "Location error",
+                        "Location error " + connectionResult.errorCode
+                    )
+                }
+                .build()
+            mGoogleApiClient?.connect()
+
+            if (mLocationRequest != null) {
+
+                val builder = LocationSettingsRequest.Builder()
+                    .addLocationRequest(mLocationRequest!!)
+
+                builder.setAlwaysShow(true)
+
+                val result = LocationServices.SettingsApi.checkLocationSettings(
+                    mGoogleApiClient,
+                    builder.build()
+                )
+                result.setResultCallback { result ->
+                    val status = result.status
+                    when (status.statusCode) {
+                        LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(activity, REQEUST_LOCATION)
+
+                        } catch (e: IntentSender.SendIntentException) {
+                            // Ignore the error.
+                        }
+
+                    }
+                }
+            }
+        }
     }
 }
