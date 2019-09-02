b1650f6c26d0286b6324246879aa47a95e655320
Suriya <mcmio125@gmail.com>
2019-09-03 00:09:56 +0700

    fix Evaluation use viewpager

--- app/src/main/java/besmart/elderlycare/screen/question/QuestionActivity.kt
+++ app/src/main/java/besmart/elderlycare/screen/question/QuestionActivity.kt
@@ -1,23 +1,14 @@
 package besmart.elderlycare.screen.question
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import besmart.elderlycare.R
 import besmart.elderlycare.model.evaluation.EvaluationResponse
 import besmart.elderlycare.model.profile.ProfileResponce
 import besmart.elderlycare.screen.base.BaseActivity
-import besmart.elderlycare.util.BaseDialog
-import kotlinx.android.synthetic.main.activity_main.recyclerView
 import kotlinx.android.synthetic.main.activity_main.toolbar
 import kotlinx.android.synthetic.main.activity_question.*
-import kotlinx.android.synthetic.main.dialog_score.view.*
-import org.koin.android.viewmodel.ext.android.viewModel
 
-class QuestionActivity : BaseActivity(), QuestionAdapter.QuestionClick {
+class QuestionActivity : BaseActivity() {
 
     companion object {
         const val EVALUATION = "evaluation"
@@ -30,15 +21,12 @@ class QuestionActivity : BaseActivity(), QuestionAdapter.QuestionClick {
     private val profile: ProfileResponce by lazy {
         intent.getParcelableExtra(PROFILE) as ProfileResponce
     }
-    private val viewModel: QuestionViewModel by viewModel()
-    private lateinit var questionAdapter: QuestionAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
 
         initInstance()
-        observerViewModel()
     }
 
     private fun initInstance() {
@@ -47,63 +35,12 @@ class QuestionActivity : BaseActivity(), QuestionAdapter.QuestionClick {
         toolbar.setNavigationOnClickListener {
             finish()
         }
-        btnSave.setOnClickListener {
-            questionAdapter.getAnswer()
-        }
-    }
 
-    private fun observerViewModel(){
-        viewModel.errorLiveData.observe(this, Observer {
-            BaseDialog.warningDialog(this, it)
-        })
-
-        viewModel.loadingLiveData.observe(this, Observer {
-            if (it) {
-                showLoadingDialog(this)
-            } else {
-                dismissDialog()
-            }
-        })
-
-        viewModel.evaluationLiveData.observe(this, Observer {
-            questionAdapter = QuestionAdapter(it.item!!, it.userEvaluarion, this)
-            recyclerView.apply {
-                layoutManager = LinearLayoutManager(this@QuestionActivity, RecyclerView.VERTICAL,  false)
-                setHasFixedSize(true)
-                adapter = questionAdapter
-            }
-        })
-
-        viewModel.scroreLiveEvent.observe(this, Observer {
-            showScoreDialog(it.result, it.getResult())
-        })
-
-        viewModel.getQuestion(evaluation.id!!, profile.cardID, evaluation.id)
-    }
-
-    override fun onError() {
-        showScoreDialog(0, "ภาวะพึ่งพาโดยสมบูรณ์")
-    }
-
-    override fun onSuccess(answer: QuestionAdapter.Answer) {
-//        showScoreDialog(answer.result, answer.getResult())
-        viewModel.addAnswer(answer, profile.cardID, evaluation.id)
-    }
-
-    @SuppressLint("InflateParams", "SetTextI18n")
-    private fun showScoreDialog(score: Int, result: String) {
-        val view = layoutInflater.inflate(R.layout.dialog_score, null)
-        val dialog = AlertDialog.Builder(this)
-        dialog.setView(view)
-
-        view.textScore.text = "$score คะแนแ"
-        view.textResult.text = result
-
-        val alertDialog = dialog.create()
-        alertDialog.show()
-
-        view.btnOK.setOnClickListener {
-            alertDialog.dismiss()
-        }
+        viewPager.adapter =
+            QuestionPagerAdapter(supportFragmentManager, evaluation = evaluation, profile = profile)
+        tabLayout.setupWithViewPager(viewPager)
+//        btnSave.setOnClickListener {
+//            questionAdapter.getAnswer()
+//        }
     }
 }
---
+++ app/src/main/java/besmart/elderlycare/screen/question/QuestionFragment.kt
@@ -0,0 +1,124 @@
+package besmart.elderlycare.screen.question
+
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import besmart.elderlycare.R
+import besmart.elderlycare.model.evaluation.EvaluationResponse
+import besmart.elderlycare.model.profile.ProfileResponce
+import besmart.elderlycare.screen.base.BaseFragment
+import besmart.elderlycare.util.BaseDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_score.view.*
+import org.koin.android.viewmodel.ext.android.viewModel
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class QuestionFragment : BaseFragment(), QuestionAdapter.QuestionClick {
+
+    companion object {
+
+        const val EVALUATION = "evaluation"
+        const val PROFILE = "profile"
+
+        fun newInstance(evaluation: EvaluationResponse, profile: ProfileResponce) =
+            QuestionFragment().apply {
+                arguments = Bundle().apply {
+                    putParcelable(EVALUATION, evaluation)
+                    putParcelable(PROFILE, profile)
+                }
+            }
+    }
+
+    private lateinit var evaluation: EvaluationResponse
+    private lateinit var profile: ProfileResponce
+    private val viewModel: QuestionViewModel by viewModel()
+    private lateinit var questionAdapter: QuestionAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.getParcelable<EvaluationResponse>(EVALUATION)?.let {
+            evaluation = it
+        }
+        arguments?.getParcelable<ProfileResponce>(PROFILE)?.let {
+            profile = it
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_question, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observerViewModel()
+    }
+
+    private fun observerViewModel(){
+        viewModel.errorLiveData.observe(this, Observer {
+            BaseDialog.warningDialog(requireContext(), it)
+        })
+
+        viewModel.loadingLiveData.observe(this, Observer {
+            if (it) {
+                showLoadingDialog(requireContext())
+            } else {
+                dismissDialog()
+            }
+        })
+
+        viewModel.evaluationLiveData.observe(this, Observer {
+            questionAdapter = QuestionAdapter(it.item!!, it.userEvaluarion, this)
+            recyclerView.apply {
+                layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL,  false)
+                setHasFixedSize(true)
+                adapter = questionAdapter
+            }
+        })
+
+        viewModel.scroreLiveEvent.observe(this, Observer {
+            showScoreDialog(it.result, it.getResult())
+        })
+
+        viewModel.getQuestion(evaluation.id!!, profile.cardID, evaluation.id)
+    }
+
+    override fun onError() {
+        showScoreDialog(0, "ภาวะพึ่งพาโดยสมบูรณ์")
+    }
+
+    override fun onSuccess(answer: QuestionAdapter.Answer) {
+//        showScoreDialog(answer.result, answer.getResult())
+        viewModel.addAnswer(answer, profile.cardID, evaluation.id)
+    }
+
+    @SuppressLint("InflateParams", "SetTextI18n")
+    private fun showScoreDialog(score: Int, result: String) {
+        val view = layoutInflater.inflate(R.layout.dialog_score, null)
+        val dialog = AlertDialog.Builder(requireContext())
+        dialog.setView(view)
+
+        view.textScore.text = "$score คะแนแ"
+        view.textResult.text = result
+
+        val alertDialog = dialog.create()
+        alertDialog.show()
+
+        view.btnOK.setOnClickListener {
+            alertDialog.dismiss()
+        }
+    }
+}
